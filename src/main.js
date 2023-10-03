11feea0be9ecf59a11ad9a5e53cc31b3c37f0f1c
Christler Neil Vinluan <christlervinluan2@gmail.com>
2023-10-03 20:41:55 +0800

    added webview and size
    adds a webview display for the window that is resizable

--- src/main.js
+++ src/main.js
@@ -17,7 +17,26 @@ let myWebview = null; // Variable to hold the webview object
 let myWebsession = null; // Variable to hold the websession object
 
 ext.runtime.onExtensionClick.addListener(async () => {
-  console.log("Extension Clicked");
+  // Get the size of the window content and create a new webview with that size
+  const size = await ext.windows.getContentSize(myWindows.id);
+
+  myWebview = await ext.webviews.create({
+    websession: myWebsession,
+  });
+
+  // Load URL in the webview and attach it to the window
+  await ext.webviews.loadURL(myWebview.id, "https://www.tldraw.com");
+
+  await ext.webviews.attach(myWebview.id, myWindows.id);
+
+  await ext.webviews.setBounds(myWebview.id, {
+    x: 0,
+    y: 0,
+    width: size.width,
+    height: size.height,
+  });
+
+  await ext.webviews.setAutoResize(myWebview.id, { width: true, height: true });
 });
 
 ext.tabs.onClicked.addListener(async () => {
